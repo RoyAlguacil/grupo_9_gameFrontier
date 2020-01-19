754ed145a9dac0b5a7f11df3c6e85f354e711bbc
RoyAlguacil <roy.alguacil@hotmail.com>
2020-01-19 16:32:50 -0300

    Cargamos productos programáticamente

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -5,70 +5,72 @@ const bcrypt = require("bcrypt");
 const rutaProductos = path.join(__dirname, "../data/products.json");
 
 const getAllProducts = () => {
-	let productosJson = fs.readFileSync(rutaProductos, "utf-8");
-	let content;
-	if (productosJson == "") {
-		content = [];
-	} else {
-		content = JSON.parse(productosJson);
-	}
-	return content;
+  let productosJson = fs.readFileSync(rutaProductos, "utf-8");
+  let content;
+  if (productosJson == "") {
+    content = [];
+  } else {
+    content = JSON.parse(productosJson);
+  }
+  return content;
 };
 
 const generateId = () => {
-	let products = getAllProducts();
-	if (products.length == 0) {
-		return 1;
-	}
-	let ultimoProducto = products.pop();
-	return ultimoProducto.id + 1;
+  let products = getAllProducts();
+  if (products.length == 0) {
+    return 1;
+  }
+  let ultimoProducto = products.pop();
+  return ultimoProducto.id + 1;
 };
 
 const guardaProducto = bodyProducto => {
-	let products = getAllProducts();
-	products.push(bodyProducto);
-	fs.writeFileSync(rutaProductos, JSON.stringify(products, null, " "));
+  let products = getAllProducts();
+  products.push(bodyProducto);
+  fs.writeFileSync(rutaProductos, JSON.stringify(products, null, " "));
 };
 
 const controller = {
-	root: (req, res) => {
-		res.render("index", {
-			title: "Home page"
-		});
-	},
-	productos: (req, res) => {
-		const id = generateId();
-		if (req.body.name && req.body.categoria && req.body.subcategoria && req.body.descripcion) {
-			guardaProducto({
-				id,
-				...req.body
-			});
-		}
-		res.render("catalog", {
-			title: "Productos",
-			productos: getAllProducts()
-		});
-	},
-	detail: (req, res) => {
-		res.render("productDetail", {
-			title: "Detalle de producto"
-		});
-	},
-	register: (req, res) => {
-		res.render("register", {
-			title: "Registro"
-		});
-	},
-	productCart: (req, res) => {
-		res.render("productCart", {
-			title: "Carrito de compras"
-		});
-	},
-	productLoad: (req, res) => {
-		res.render("productLoad", {
-			title: "Carga de Producto"
-		});
-	}
+  root: (req, res) => {
+    res.render("index", {
+      title: "Home page"
+    });
+  },
+  productos: (req, res) => {
+    console.log(req.body);
+    const id = generateId();
+    guardaProducto({
+      id,
+      image: req.file ? req.file.filename : null,
+      ...req.body
+    });
+    console.log(req.body);
+
+    res.render("catalog", {
+      title: "Productos",
+      productos: getAllProducts()
+    });
+  },
+  detail: (req, res) => {
+    res.render("productDetail", {
+      title: "Detalle de producto"
+    });
+  },
+  register: (req, res) => {
+    res.render("register", {
+      title: "Registro"
+    });
+  },
+  productCart: (req, res) => {
+    res.render("productCart", {
+      title: "Carrito de compras"
+    });
+  },
+  productLoad: (req, res) => {
+    res.render("productLoad", {
+      title: "Carga de Producto"
+    });
+  }
 };
 
 module.exports = controller;
--- src/routes/main.js
+++ src/routes/main.js
@@ -1,32 +1,50 @@
 // ************ Require's ************
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const multer = require("multer");
+const path = require("path");
+
+let storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, path.join(__dirname, "../../public/images/multer"));
+  },
+  filename: function(req, file, cb) {
+    let finalName = Date.now() + path.extname(file.originalname);
+    cb(null, finalName);
+  }
+});
+
+let upload = multer({ storage: storage });
 
 // ************ Controller Require ************
-const mainController = require('../controllers/mainController');
+const mainController = require("../controllers/mainController");
 
 /* GET - home page. */
-router.get('/', mainController.root);
+router.get("/", mainController.root);
 
 /* GET - Pagina de Registracion*/
-router.get('/registro', mainController.register);
+router.get("/registro", mainController.register);
 
 /* Detalle de Producto */
 
-router.get('/detalle', mainController.detail);
+router.get("/detalle", mainController.detail);
 
 /* GET - Catálogo*/
-router.get('/productos', mainController.productos);
+router.get("/productos", mainController.productos);
 
-/* POST - catálogo*/
-router.post('/productos', mainController.productos);
+/* POST - Catálogo*/
+router.post(
+  "/productos",
+  upload.single("image_input"),
+  mainController.productos
+);
 
 /* Carrito de Compras */
 
-router.get('/carrito', mainController.productCart)
+router.get("/carrito", mainController.productCart);
 
 /* Carrito de Carga */
 
-router.get('/carga-producto', mainController.productLoad)
+router.get("/carga-producto", mainController.productLoad);
 
 module.exports = router;
