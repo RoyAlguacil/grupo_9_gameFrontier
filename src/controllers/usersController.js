3eadcf9e582475ea03b1284a261430898ef28833
RoyAlguacil <roy.alguacil@hotmail.com>
2020-01-23 21:07:21 -0300

    creamos el form de login

---
+++ src/controllers/usersController.js
@@ -0,0 +1,112 @@
+const fs = require('fs');
+const path = require('path');
+const bcrypt = require('bcrypt');
+
+// Users File Path
+const usersFilePath = path.join(__dirname, '../data/users.json');
+
+// Helper Functions
+function getAllUsers() {
+    let usersFileContent = fs.readFileSync(usersFilePath, 'utf-8');
+    let usersArray;
+    if (usersFileContent == '') {
+        usersArray = [];
+    } else {
+        usersArray = JSON.parse(usersFileContent);
+    }
+    return usersArray;
+}
+
+function generateId() {
+    let users = getAllUsers();
+    if (users.length == 0) {
+        return 1;
+    }
+    let lastUser = users.pop();
+    return lastUser.id + 1;
+}
+
+function storeUser(userData) {
+    let users = getAllUsers();
+    users.push(userData);
+    fs.writeFileSync(usersFilePath, JSON.stringify(users, null, ' '));
+}
+
+function getUserByEmail(email) {
+    let allUsers = getAllUsers();
+    let userFind = allUsers.find(oneUser => oneUser.user_email == email);
+    return userFind;
+}
+
+
+const controller = {
+    loginForm: (req, res) => {
+        res.render('users/loginForm');
+    },
+    storeUser: (req, res) => {
+        // Hasheo la contraseña
+        req.body.user_password = bcrypt.hashSync(req.body.user_password, 11);
+        // Genero la data del usuario
+        let newUserData = {
+            id: generateId(),
+            avatar: req.file.filename,
+            ...req.body
+        }
+        // Guardo al usuario en el JSON
+        storeUser(newUserData);
+        // Redirección
+        res.redirect('/');
+    },
+    loginForm: (req, res) => {
+        res.render('users/loginForm', {
+            title: 'Pagina Login'
+        });
+    },
+    processLogin: (req, res) => {
+        // Busco al usuario por email
+        let userToLogin = getUserByEmail(req.body.user_email);
+
+        // Valido si existe el usuario
+        if (userToLogin != undefined) {
+            // Magia
+            if (bcrypt.compareSync(req.body.user_password, userToLogin.user_password)) {
+                // Borramos la contraseña del objeto usuario
+                delete userToLogin.user_password;
+
+                // Pasamos al usuario a session
+                req.session.user = userToLogin;
+
+                if (req.body.remember) {
+                    res.cookie('user', userToLogin.id, {
+                        maxAge: 180000
+                    });
+                }
+
+                // Redirección
+                return res.redirect('/users/profile');
+            } else {
+                res.send('Datos incorrectos');
+            }
+        } else {
+            return res.redirect('/users/register');
+        }
+    },
+    profile: (req, res) => {
+        res.render('users/profile', {
+            user: req.session.user
+        });
+    },
+    logout: (req, res) => {
+        // Destruimos la session
+        req.session.destroy();
+        // Pisar la cookie
+        res.cookie('user', null, {
+            maxAge: -1
+        });
+        // Redirección
+        return res.redirect('/users/loginForm');
+    }
+
+};
+
+module.exports = controller
--- src/routes/main.js
+++ src/routes/main.js
@@ -20,6 +20,7 @@ let upload = multer({
 
 // ************ Controller Require ************
 const mainController = require("../controllers/mainController");
+const usersController = require("../controllers/usersController");
 
 /******
  * Productos
@@ -29,7 +30,7 @@ const mainController = require("../controllers/mainController");
 router.get("/", mainController.root);
 
 /* Registro GET*/
-router.get("/registro", mainController.register);
+router.get("/users/register", mainController.register);
 
 /* Products obtain --> GET */
 router.get("/productos", mainController.productos);
@@ -46,6 +47,12 @@ router.post(
     upload.single("image_input"),
     mainController.productos
 );
+
+/* Formulario de Login --> GET */
+router.get("/users/loginForm", usersController.loginForm);
+
+/* Formulario de Login --> GET */
+router.post("/users/register", usersController.processLogin)
 
 /* Products update --> POST */
 router.get("/productos/editar/:id", mainController.update);
