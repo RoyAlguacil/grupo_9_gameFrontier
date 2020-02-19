3edab4e403157125e176a453025f71d6a9b652e8
JuanAguilera1980 <jota.pe2202@gmail.com>
2020-02-18 22:12:29 -0300

    probando validator

--- public/js/registerValidator.js
+++ public/js/registerValidator.js
@@ -1,22 +1,26 @@
-window.onload = function() {
-   const formulario = document.querySelector('form');
-   const inputs = Array.from(formulario.elements);
-   inputs.pop();
+window.onload = function () {
+    const formulario = document.querySelector('form');
+    const inputs = Array.from(formulario.elements);
+    inputs.pop();
 
-   const errores = {};
-   // Añadir eventos
-   inputs.forEach(input => {
-       if(input.classList.contains('avatar')) {
-          input.addEventListener('change', e => {
-              console.log('avatar');
-          })
-        } else {
-            input.addEventListener('blur', () => {
-                console.log('no');
-            })
-        }
+    const errores = {};
+    // Añadir eventos
+    inputs.forEach(function (input) {
+        input.addEventListener('blur', function () {
+            let valorInput = this.value;
+            if (validator.isEmpty(valorInput, { ignore_whitespace: true })) {
+                this.classList.add('is-invalid');
+                this.nextElementSibling.innerHTML = `El campo "${this.dataset.name}" es obligatorio`;
+                errores[this.name] = true;
+                console.log(errores);
+            } else {
+                this.classList.remove('is-invalid');
+                this.classList.add('is-valid');
+                this.nextElementSibling.innerHTML = ``;
+                delete errore[this.name];
+                console.log(errores);
+            }
+        })
     })
-console.log(validator);
-
-
+    // console.log(validator);
 }
