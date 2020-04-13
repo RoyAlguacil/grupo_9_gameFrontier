bdaaf8c3e2b8cc588237017a616970024f5c6e25
RoyAlguacil <hiteplegold@gmail.com>
2020-04-12 21:12:00 -0300

    se agrega la suma de productos

--- src/middlewares/registerValidations.js
+++ src/middlewares/registerValidations.js
@@ -2,13 +2,13 @@ const { check } = require('express-validator');
 
 module.exports = [
 check('usuario').notEmpty().withMessage('Este campo es obligatorio').bail(),
-check('usuario').isLength({min:6}).withMessage('Debe contener al menos 6 caracteres'),
+check('usuario').isLength({min:4}).withMessage('Debe contener al menos 4 caracteres'),
 
 check('password').notEmpty().withMessage('Este campo es obligatorio').bail(),
 check('password').isAlphanumeric().withMessage('La contraseña solo debe contener letras y números'),
 
 check('nombre').notEmpty().withMessage('Este campo es obligatorio').bail(),
-check('nombre').isLength({min:6}).withMessage('Debe contener al menos 6 caracteres'),
+check('nombre').isLength({min:4}).withMessage('Debe contener al menos 4 caracteres'),
 
 check('telefono').notEmpty().withMessage('Este campo es obligatorio').bail(),
 check('telefono').isLength({min:8, max:13}).withMessage('Debe contener al menos 8 números').bail(),
@@ -18,7 +18,7 @@ check('provincia').notEmpty().withMessage('Este campo es obligatorio').bail(),
 check('provincia').isLength({min:3, max:15}).withMessage('Debe contener al menos 3 caracteres'),
 
 check('localidad').notEmpty().withMessage('Este campo es obligatorio').bail(),
-check('localidad').isLength({min:6}).withMessage('Debe contener al menos 6 caracteres'),
+check('localidad').isLength({min:4}).withMessage('Debe contener al menos 4 caracteres'),
 
 check('dni').notEmpty().withMessage('Este campo es obligatorio').bail(),
 check('dni').isLength({min:8, max:8}).withMessage('Debe contener 8 dígitos').bail(),
